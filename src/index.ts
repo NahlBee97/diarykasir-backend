45baf28b0e550f4bfe1fb38321bb9eda778a1b36
Nahalil <nahalilmuchtar2@gmail.com>
2025-12-20 19:46:39 +0800

    vercel config

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,7 @@
-import express, { Request, Response } from "express";
-import { errorHandler } from "./middlewares/errorHandler";
-import cors from "cors";
-import dotenv from "dotenv";
-import path from "path"
+import app from "./app";
 
-dotenv.config();
+const PORT = process.env.PORT || 5000;
 
-const app = express();
-
-const port = process.env.PORT || 5000;
-
-// Routes
-import authRoutes from "./routes/authRoutes";
-import productRoutes from "./routes/productRoutes";
-import cartRoutes from "./routes/cartRoutes";
-import orderRoutes from "./routes/orderRoutes";
-
-// cors
-app.use(
-  cors({
-    origin: process.env.FE_URL,
-    credentials: true,
-  })
-);
-
-app.use(express.json());
-app.use(express.static(path.join(process.cwd(), "public")));
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("Diary Kasir Backend Connected");
-});
-
-app.use("/api/auth", authRoutes);
-app.use("/api/products", productRoutes);
-app.use("/api/carts", cartRoutes);
-app.use("/api/orders", orderRoutes);
-
-// Error Handler
-app.use(errorHandler);
-
-app.listen(port, () => {
-  console.log(`Backend server is running on http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Backend server is running on http://localhost:${PORT}`);
 });
